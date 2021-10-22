dc2fb626d3296054f55533b1ffbfa77e7d63d0a4
tc8appdevelo <tc8appdevelo@gmail.com>
2021-10-22 12:35:47 -0500

    finished PolyTreeNode#parent=()

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -14,10 +14,34 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        if !has_node?(node.value)
-            @parent = node.value
-            @children << node.value
+
+        if @parent != nil && node == nil
+            idx = @parent.children.index(self)
+            @parent.children.delete_at(idx)
+            @parent = nil
         end
+
+        if @parent == nil && node == nil
+            return
+        end
+        
+        if @parent == nil
+            @parent = node
+            node.children << self #if node.childre.include?(self)
+        else
+            idx = @parent.children.index(self)
+            @parent.children.delete_at(idx)
+            node.children << self if !node.children.include?(self)
+            @parent = node
+        end
+
     end
 
+    
+
+
 end
+
+#     a.children
+#    b c
+#   d e f
