9aec9050ba440ee06ccc2baff8efee48dff67abf
tc8appdevelo <tc8appdevelo@gmail.com>
2021-10-22 10:35:59 -0500

    created initialize method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
 
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+
+    # def has_node?(node)
+    #     @children.include?(node)
+    # end
+
+    # def parent=(node)
+    #     if !has_node?(node)
+            
+    # end
+
 end
